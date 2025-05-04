44dcf841abab148bc1082d88e1dde2967aa1fd7c
russeree <git@qrsnap.io>
2025-05-04 07:08:36 -0700

    [debug] Print spks

--- crates/chain/src/indexer/keychain_txout.rs
+++ crates/chain/src/indexer/keychain_txout.rs
@@ -441,6 +441,8 @@ impl<K: Clone + Ord + Debug> KeychainTxOutIndex<K> {
     /// Syncs the state of the inner spk index after changes to a keychain
     /// Will utilize the replenish cache if availble to bypass the derivation burden existing derived spks
     fn replenish_inner_index(&mut self, did: DescriptorId, keychain: &K, lookahead: u32) {
+        let revealed_spks= self.inner.all_spks();
+        println!("Table of revealed spks is {:?}", revealed_spks);
         let descriptor = self.descriptors.get(&did).expect("invariant");
         let next_store_index = self
             .inner
